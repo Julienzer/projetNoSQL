7134693efb4d30c624cd641ad7923bc89f66154a
Julienzer <julien.2001@hotmail.fr>
2023-11-22 19:05:14 +0100

    premier commit

---
+++ front-projet-nosql/src/_AjoutArticle/addArticle.tsx
@@ -0,0 +1,19 @@
+import './addArticle.css'
+
+function addArticle() {
+
+
+  return (
+    <>
+      <h1>Nouvel article</h1>
+      <div className="card">
+        <button>
+          Ajouter
+        </button>
+      </div>
+
+    </>
+  )
+}
+
+export default addArticle
---
+++ server/src/main/java/com/example/projetNoSQL/ProjetNoSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetNoSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetNoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetNoSqlApplication.class, args);
+	}
+
+}
