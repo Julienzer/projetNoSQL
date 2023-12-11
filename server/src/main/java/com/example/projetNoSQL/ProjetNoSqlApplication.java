86da093d2f061b1b7d9fbc5a09ded1a0a97478c6
Clement Luke <clement.luke@free.fr>
2023-12-11 19:21:39 +0100

    Creation Entity et du Repo

--- server/src/main/java/com/example/projetNoSQL/ProjetNoSqlApplication.java
+++ server/src/main/java/com/example/projetNoSQL/ProjetNoSqlApplication.java
@@ -10,4 +10,5 @@ public class ProjetNoSqlApplication {
 		SpringApplication.run(ProjetNoSqlApplication.class, args);
 	}
 
+
 }
---
+++ server/src/main/java/com/example/projetNoSQL/repository/ArticleRepository.java
@@ -0,0 +1,5 @@
+package com.example.projetNoSQL.repository;
+
+
+public class ArticleRepository {
+}
